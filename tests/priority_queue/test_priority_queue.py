87b2b48f60f21d540ccd71949a922dab9359ae80
D-Wolter <wolter.daniel@gmail.com>
2023-06-14 14:43:09 -0300

    feat: req6 criado o teste de priority_queue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,32 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    pq = PriorityQueue()
+
+    first = {'qtd_linhas': 3}
+    second = {'qtd_linhas': 7}
+    third = {'qtd_linhas': 4}
+    fourth = {'qtd_linhas': 9}
+
+    pq.enqueue(first)
+    pq.enqueue(second)
+    pq.enqueue(third)
+    pq.enqueue(fourth)
+
+    assert len(pq) == 4
+
+    assert pq.search(0) == {"qtd_linhas": 3}
+    assert pq.search(1) == {"qtd_linhas": 4}
+    assert pq.search(2) == {"qtd_linhas": 7}
+    assert pq.search(3) == {"qtd_linhas": 9}
+
+    assert pq.dequeue() == {'qtd_linhas': 3}
+
+    assert len(pq) == 3
+
+    with pytest.raises(IndexError, match='Índice Inválido ou Inexistente'):
+        pq.search(-11)
+    with pytest.raises(IndexError, match='Índice Inválido ou Inexistente'):
+        pq.search(11)
